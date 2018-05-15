e8bc63ef137a9a882e60b493b7d8bab25e7b38e3
Floor01010 <35524575+Floor01010@users.noreply.github.com>
2018-05-15 15:09:35 +0200

    initial commit

---
+++ src/com/company/Cell.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Cell {
+	private CellState state;
+	public Cell(CellState state){
+		this.state = state;
+	}
+	public CellState getState() {
+		return state;
+	}
+
+	public void setState(CellState state) {
+		this.state = state;
+	}
+}
